2406b5dd6e893573632f2543d5497c086b2f41b0
Maih-design <themaih.dev@gmail.com>
2026-08-15 13:33:42 +0300

    bat fix

--- GamaKnifeSystem_start.bat
+++ GamaKnifeSystem_start.bat
@@ -4,10 +4,8 @@ cd /d "C:\Users\mai_h\Gama3.0"
 
 call venv\Scripts\activate.bat
 
-start "" cmd /c "python manage.py runserver"
-
-timeout /t 2 /nobreak >nul
+timeout /t 1 /nobreak >nul
 
 start "" http://127.0.0.1:8000
 
-exit
+python manage.py runserver
